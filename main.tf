1e4c8bb84739912808a581927936a77c55cf2422
ryuinhye95@gmail.com <root@ip-10-50-1-254.ap-northeast-2.compute.internal>
2023-08-09 09:00:53 +0000

    modifed tf setting

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 module "eks" {
   source = "./eks"
 
-  cluster_version    = "1.22"
+  cluster_version    = "1.24"
   vpc_id             = module.vpc.vpc_id
   public_subnet_ids  = module.vpc.vpc_public_subnet_ids
   private_subnet_ids = module.vpc.vpc_private_subnet_ids
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "cluster_name" {
   type        = string
   description = "eks cluster name"
-  default     = "eks-test"
+  default     = "eks-cluster-t1"
 }
